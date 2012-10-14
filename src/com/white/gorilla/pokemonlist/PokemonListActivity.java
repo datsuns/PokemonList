3f048718d0de65a6a8655f70e4666a7a231e8e1a
datsuns <the.skeleton7@gmail.com>
2012-10-14 18:29:53 +0900

    文字コードの怪しそうなコメントを削除

--- src/com/white/gorilla/pokemonlist/PokemonListActivity.java
+++ src/com/white/gorilla/pokemonlist/PokemonListActivity.java
@@ -34,7 +34,6 @@ public class PokemonListActivity extends Activity {
         v.setAdapter(adapter);
         v.setOnItemClickListener(new AdapterView.OnItemClickListener() {
         	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // クリックされたアイテムを取得します
                 Intent intent = new Intent( getApplicationContext(), PokemonDetailView.class );
                 intent.putExtra( "ItemNumber", position );
                 startActivity( intent );
